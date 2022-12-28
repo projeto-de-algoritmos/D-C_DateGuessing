25b34a225bf7008848c2a7c9e2cdfc258bd576f6
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-28 12:06:43 -0300

    feat: adding date guessing
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,98 +1,190 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
-import moment from 'moment';
 import Date from '../assets/date.png'
 import './index.css';
 
-const upperLimit = {
-    year: 2030,
-    month: 12,
-    day: 31
-};
-const lowerLimit = {
-    year: 2000,
-    month: 12,
-    day: 31
-};
+let upperDayLimit = 32;
+let lowerDayLimit = 1;
+let upperMonthLimit = 12;
+let lowerMonthLimit = 1;
+let upperYearLimit = 2100;
+let lowerYearLimit = 1990;
+
+const months = [
+    {
+        num: 1,
+        nome: "Janeiro"
+    },
+    {
+        num: 2,
+        nome: "Feveveiro"
+    },
+    {
+        num: 3,
+        nome: "Março"
+    },
+    {
+        num: 4,
+        nome: "Abirl"
+    },
+    {
+        num: 5,
+        nome: "Maio"
+    },
+    {
+        num: 6,
+        nome: "Junho"
+    },
+    {
+        num: 7,
+        nome: "Julho"
+    },
+    {
+        num: 8,
+        nome: "Agosto"
+    },
+    {
+        num: 9,
+        nome: "Setembro"
+    },
+    {
+        num: 10,
+        nome: "Outubro"
+    },
+    {
+        num: 11,
+        nome: "Novembro"
+    },
+    {
+        num: 12,
+        nome: "Dezembro"
+    },
+
+]
 
 const Home = () => {
-    const [date, setDate] = useState({
-        year: null,
-        month: null,
-        day: null
+    const [medianaDate, setMedianaDate] = useState({
+        year: Math.trunc((upperYearLimit + lowerYearLimit) / 2),
+        month: Math.ceil((upperMonthLimit + lowerMonthLimit) / 2),
+        day: Math.ceil((upperDayLimit + lowerDayLimit) / 2),
     });
     const [rightDate, setRightDate] = useState(false);
-
-    useEffect(() => {
-        handleInitialValues();
-    }, []);
+    const [count, setCount] = useState(0);
 
     const handleInitialValues = () => {
-        const currentDate = moment();
-        setDate({
-            year: currentDate.year(),
-            month: currentDate.month(),
-            day: currentDate.date()
+        setMedianaDate({
+            year: Math.trunc((upperYearLimit + lowerYearLimit) / 2),
+            month: Math.ceil((upperMonthLimit + lowerMonthLimit) / 2),
+            day: Math.ceil((upperDayLimit + lowerDayLimit) / 2),
         });
+        setCount(0);
         setRightDate(false);
+
+    }
+    const handleUpperLimit = (type) => {
+        console.log("mediana date", medianaDate)
+        if (type === 'day') {
+            lowerDayLimit = medianaDate.day;
+            setMedianaDate(prevState => ({ ...prevState, day: Math.trunc((upperDayLimit + lowerDayLimit) / 2) }))
+        }
+        if (type === 'month') {
+            lowerMonthLimit = medianaDate.month;
+            setMedianaDate(prevState => ({ ...prevState, month: Math.ceil((upperMonthLimit + lowerMonthLimit) / 2) }))
+        }
+        if (type === 'year') {
+            lowerYearLimit = medianaDate.year;
+            setMedianaDate(prevState => ({ ...prevState, year: Math.ceil((upperYearLimit + lowerYearLimit) / 2) }))
+        }
+        setCount(count + 1);
+        console.log("mediana date", medianaDate)
     }
 
-    const handleUpperLimit = () => {
-        const sumOfDatesYear = moment(`${upperLimit.year}-${upperLimit.month}-${upperLimit.day}`).add(date.year, 'years');
-        console.log("sumOfDatesYear0", sumOfDatesYear.date(), sumOfDatesYear.month(), sumOfDatesYear.year())
-        const sumOfDatesMonth = moment(`${sumOfDatesYear.year()}-${sumOfDatesYear.month()}-${sumOfDatesYear.day()}`).add(date.month, 'months');
-        console.log("sumOfDatesYear1",sumOfDatesMonth.date(), sumOfDatesMonth.month(), sumOfDatesMonth.year())
-        const sumOfDatesDays = moment(`${sumOfDatesMonth.year()}-${sumOfDatesMonth.month()}-${sumOfDatesMonth.day()}`).add(date.day, 'days');
-        console.log("sumOfDatesYear2", sumOfDatesDays.date(), sumOfDatesDays.month(), sumOfDatesDays.year())
-        setDate({
-            year: sumOfDatesDays.year(),
-            month: sumOfDatesDays.month(),
-            day: sumOfDatesDays.date()
-        });
+    const handlelowerLimit = (type) => {
+        console.log("mediana date", medianaDate)
+
+        if (type === 'day') {
+            upperDayLimit = medianaDate.day;
+            setMedianaDate(prevState => ({ ...prevState, day: Math.trunc((upperDayLimit + lowerDayLimit) / 2) }))
+        }
+        if (type === 'month') {
+            upperMonthLimit = medianaDate.month;
+            setMedianaDate(prevState => ({ ...prevState, month: Math.trunc((upperMonthLimit + lowerMonthLimit) / 2) }))
+        }
+        if (type === 'year') {
+            upperYearLimit = medianaDate.year;
+            setMedianaDate(prevState => ({ ...prevState, year: Math.trunc((upperYearLimit + lowerYearLimit) / 2) }))
+        }
+        setCount(count + 1);
+        console.log("mediana date", medianaDate)
     }
 
-    const handlelowerLimit = () => {
-
+    const handleMonth = () => {
+        const currentMont = (months.find(m => m.num == medianaDate.month)).nome;
+        return currentMont
     }
 
     const handleCorrectDate = () => {
         setRightDate(true);
     }
 
-    const currentDate = moment();
-
     return (
         <div className="home-container centraliza">
             <div className="home-main centraliza mb-5">
                 <div className="home-header">
                     <h1>Date Guessing <img src={Date} alt="Date img" width="40px" /></h1>
-                    <p>Pense em uma data entre os anos 2000 e 2100 que iremos adivinhar.</p>
+                    <p>Pense em uma data entre as datas 1 de Janeiro 1990 até 31 de dezembro de 2100 que iremos adivinhar.</p>
                     {rightDate && <Alert variant='success' className="centraliza">
-                        Chegamos a data correta! Foram necessárias 10 tentativas.
+                        Chegamos a data correta! Foram necessárias {count} tentativas.
                     </Alert>}
                 </div>
                 <div className="home-content centraliza pt-3 mt-3">
                     <h2 className="pt-4">Nosso Palpite</h2>
                     <div className="home-content-date mt-4">
                         <div className="row label centraliza">
-                            <div className="col-md-4 col-12 centraliza">Dia</div>
-                            <div className="col-md-4 col-12 centraliza">Mês</div>
-                            <div className="col-md-4 col-12 centraliza">Ano</div>
+                            <div className="col-4 centraliza">Dia</div>
+                            <div className="col-4 centraliza">Mês</div>
+                            <div className="col-4 centraliza">Ano</div>
                         </div>
                         <div className="row date centraliza">
-                            <div className="col-md-4 col-12 centraliza">{date.day}</div>
-                            <div className="col-md-4 col-12 centraliza">{date.month}</div>
-                            <div className="col-md-4 col-12 centraliza">{date.year}</div>
+                            <div className="col-4 centraliza date-card">
+                                <div className="row centraliza">
+                                    <div className="col-7 day">
+                                        {medianaDate.day}
+                                    </div>
+                                    <div className="col-5">
+                                        <button id="previous" onClick={() => handlelowerLimit('day')}> Anterior</button>
+                                        <button id="next" onClick={() => handleUpperLimit('day')}> Posterior</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-4 centraliza date-card">
+                                <div className="row centraliza">
+                                    <div className="col-7 month">
+                                        {handleMonth()}
+                                    </div>
+                                    <div className="col-5">
+                                        <button id="previous" onClick={() => handlelowerLimit('month')}> Anterior</button>
+                                        <button id="next" onClick={() => handleUpperLimit('month')}> Posterior</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-4 centraliza date-card">
+                                <div className="row centraliza">
+                                    <div className="col-7 year">
+                                        {medianaDate.year}
+                                    </div>
+                                    <div className="col-5">
+                                        <button id="previous" onClick={() => handlelowerLimit('year')}> Anterior</button>
+                                        <button id="next" onClick={() => handleUpperLimit('year')}> Posterior</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="home-content-buttons centraliza mt-5">
                         <div className="home-content-buttons-interactive">
-                            <button id="previous"> Anterior</button>
-                            <button id="next" onClick={() => handleUpperLimit()}> Posterior</button>
-                        </div>
-                        <div className="home-content-buttons-interactive">
-                            <button id="right" onClick={() => handleCorrectDate()}> Acertou</button>
-                            <button id="reset" onClick={() => handleInitialValues()}> Zerar</button>
+                            <button id="right" className="final-button" onClick={() => handleCorrectDate()}> Finalizar</button>
+                            <button id="reset" className="final-button" onClick={() => handleInitialValues()}> Reiniciar</button>
                         </div>
                     </div>
                 </div>
